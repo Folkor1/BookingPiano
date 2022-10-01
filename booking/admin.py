5a5e03aabb4c1c8c9587fa6062d69227884797d4
Folkor1 <igorvasiljevs@gmail.com>
2022-10-01 17:57:48 +0000

    Add lesson model

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Lesson
 
-# Register your models here.
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    """
+    Class for Lesson Admin model
+    """
+
+    list_display = ('get_lesson_display', 'get_lesson_type_display', 'date', 'time', 'owner')
+    search_fields = ['date', 'owner']
+    list_filter = ('lesson', 'lesson_type', 'date')
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+LESSONS = (('lessons_1', 'Piano'),
+           ('lessons_2', 'Theory'))
+
+LESSON_TYPE = (('lesson_type1', 'Online'),
+               ('lesson_type2', 'Offline'))
+
+class Lesson(models.Model):
+    """
+    Class for lessons model.
+    """
+    lesson = MultiSelectField(choices=LESSONS, max_choices=1)
+    lesson_type = MultiSelectField(choices=LESSON_TYPE, max_choices=1)
+    date = models.DateField()
+    time = models.TimeField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lessons")
+
+    class Meta:
+        ordering = ['-date']
+        unique_together = ("date", "time")
+
+    def __str__(self):
+        return self.get_lesson_display()
